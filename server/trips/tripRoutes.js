9d4bee452e5aa3710950e25cda4e61675e8b2a28
Tim Dang <timothy.dang@gmail.com>
2016-02-19 14:48:35 -0800

    staying ahead of the git monster

--- server/trips/tripRoutes.js
+++ server/trips/tripRoutes.js
@@ -4,4 +4,5 @@ module.exports = function(app) {
   app.post('/create', tripController.create);
   app.post('/remove',tripController.remove);
   app.post('/modify/', tripController.modify);
+  // app.get('/modify/:routeID', userController.checkAuth, tripController.modify);
 };
